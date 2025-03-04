cf63dbf83d6a2d2e86fd613848538db69ca6e43a
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-03-04 18:25:40 +0100

    add 2d-portfolio app to the folder

---
+++ 2d-portfolio/src/constants.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC.`,
+  "cs-degree": `This is my CS degree.`,
+  "sofa-table": `That's my sofa.`,
+  tv: `That's my TV.`,
+  bed: `This is my bed.`,
+  resume: `This is my desk and on it there's my CV.`,
+  projects: `Info about this portfolio.`,
+  library: `There are a lot of books on my shelves.`,
+  exit: `If you want to exit this portfolio just close the tab.` 
+}
